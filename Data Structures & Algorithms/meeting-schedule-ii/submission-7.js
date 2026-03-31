3246959d8e76bc32caf35d0cc7b32553640db299
org-prime-factor <theshahedali@gmail.com>
2026-03-31 19:29:24 +0530

    Add: meeting-schedule-ii - submission-7

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-7.js
@@ -0,0 +1,38 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ *   constructor(start, end) {
+ *     this.start = start;
+ *     this.end = end;
+ *   }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {Interval[]} intervals
+     * @returns {number}
+     */
+    minMeetingRooms(intervals) {
+        const starts = intervals.map(i=>i.start).sort((a,b)=>a-b);
+        const ends = intervals.map(i=>i.end).sort((a,b)=>a-b);
+
+        let rooms = 0, maxRooms = 0;
+        let s = 0, e = 0;
+    
+        while (s < intervals.length) {
+            if (starts[s] < ends[e]) {
+                rooms++;
+                s++;
+            } else {
+                rooms--;
+                e++;
+            }
+            maxRooms = Math.max(maxRooms, rooms);
+        }
+        
+        return maxRooms;
+
+
+    }
+}
